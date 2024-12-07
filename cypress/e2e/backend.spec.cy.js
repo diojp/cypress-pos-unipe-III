19428b28e8a5023b40fbdcbf157a287c6d2fa740
Rafael Ramos <pe.rafael.anderson@gmail.com>
2024-12-07 13:26:03 -0300

    Pipeline

--- cypress/e2e/backend.spec.cy.js
+++ cypress/e2e/backend.spec.cy.js
@@ -10,4 +10,30 @@ describe("Testes de Back", () => {
             expect(statusText).to.be.equal('OK');
         })
     })
+
+    it("TC02 - Requisição POST", () => {
+        cy.request({
+            method: 'POST',
+            url: 'https://httpbin.org/post',
+            body: {
+                name: 'Rafael',
+                age: '30'
+            }
+        }).should(function(response){
+            const { status, statusText } = response;
+            expect(status).to.be.equal(200);
+            expect(statusText).to.be.equal('OK');
+        })
+    })
+
+    it("TC03 - Requisição POST", () => {
+        cy.request({
+            method: 'POST',
+            url: 'https://httpbin.org/post',
+            body: {
+                name: 'Rafael',
+                age: '30'
+            }
+        }).its('body').should('not.be.empty')
+    })
 })
